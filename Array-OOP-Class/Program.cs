662939d2d405c788d54db01268fc91a5160a21f7
javidancode <javidanig@code.edu.az>
2022-06-23 02:29:45 +0400

    Added Array-OOP-Class-practica-homewok table

---
+++ Array-OOP-Class/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Array_OOP_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name1 = "Cavidan";
+            //string surname1 = "Qedirli";
+            //int age1 = 17;
+
+            //Console.WriteLine(name1 + " " + surname1);
+
+
+            //var obj1 = new
+            //{
+            //    name = "Cavidan",
+            //    surname = "Qedirli",
+            //    age = 17
+
+
+            //};
+
+            //Console.WriteLine(obj1.name + " " + obj1.surname);
+
+
+
+            //Student st1 = new Student("Cavidan");
+            //st1.name = "Cavidan";
+            //st1.surname = "Qedirli";
+            //st1.age = 17;
+
+            //Console.WriteLine(st1.FullData());
+            //Console.WriteLine(st1.FullName());
+
+            //Student st2 = new Student
+            //{
+            //    name = "Cavidan",
+            //    surname = "Qedirli",
+            //    age = 17
+            //};
+
+
+        }
+    }
+
+    class Student
+    {
+        //public string name;
+        //public string surname;
+        //public int age;
+        //public string address;
+
+
+        //public Student()
+        //{
+        //    Console.WriteLine("Empty");
+        //}
+
+        //public Student(int age)
+        //{
+        //    Console.WriteLine("age:" + age);
+        //}
+
+        //public Student(string stuNmae)
+        //{
+        //    Console.WriteLine("name:" + stuNmae);
+        //}
+
+
+
+
+
+
+
+
+
+
+        //public Student(string studentName, int studentAge)
+        //{
+        //    name = studentName;
+        //    age = studentAge;
+        //    Console.WriteLine(name + " " + age);
+        //}
+
+
+
+
+
+
+        //public Student()
+        //{
+        //    Console.WriteLine("constructor");
+        //}
+
+
+        //public string FullName()
+        //{
+        //    return name + " " + surname;
+        //}
+
+        //public string FullData()
+        //{
+        //    return name + " " + surname + " " + age;
+        //}
+
+    }
+}
